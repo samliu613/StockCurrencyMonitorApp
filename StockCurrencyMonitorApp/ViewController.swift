10657844a7110f2f36f77aa3ae898fe476c14e18
samliu613 <samliu613@hotmail.com>
2018-10-21 20:56:52 +0200

    Initial Commit

---
+++ StockCurrencyMonitorApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StockCurrencyMonitorApp
+//
+//  Created by Yongsen on 2018/10/21.
+//  Copyright © 2018 SAMDev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
